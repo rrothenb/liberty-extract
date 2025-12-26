dee04e535d0c556441511a5c15184f2e4a84f243
Rick Rothenberg <rrothenb@yahoo.com>
2025-12-26 16:27:19 +0100

    Add resources and start working on writing out CSV and do some cleanup (I know I have stuff to fix)

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: '.',
-  testMatch: 'borrowers.ts',
+  testMatch: '*.ts',
 });
---
+++ resources.ts
@@ -0,0 +1,51 @@
+import {test, Page} from '@playwright/test'
+import ObjectsToCsv from 'objects-to-csv'
+
+let resourceNumber = 0
+
+async function nextResource (page: Page) {
+  await page.getByTitle(`Item ${++resourceNumber}`).click()
+}
+
+test('Fetch Resources', async ({page}) => {
+  try {
+    await page.goto(`http://${process.env.HOST}/liberty/libraryHome.do`)
+    await page.getByRole('button', {name: 'Login'}).click()
+    await page.getByRole('textbox', {name: 'Username:'}).click()
+    await page.getByRole('textbox', {name: 'Username:'}).fill(process.env.USERNAME!)
+    await page.getByRole('textbox', {name: 'Password:'}).click()
+    await page.getByRole('textbox', {name: 'Password:'}).fill(process.env.PASSWORD!)
+    await page.getByRole('button', {name: 'Login'}).click()
+    await page.goto(`http://${process.env.HOST}/liberty/cataloguing/biblios/browse.do`)
+    await page.locator('#navigation_message a').click()
+    const navMsg = await page.locator('#navigation_message').allInnerTexts()
+    console.log(navMsg[0].split(' ')[2])
+    resourceNumber = 1
+    await page.getByRole('link', {name: `${resourceNumber}`, exact: true}).click()
+    const resources = []
+    for (let i=0;i<5;i++) {
+      // Print text from specific elements (e.g., all table cells)
+      const cells = await page.locator('tr').allInnerTexts()
+      resources.push(cells
+        .filter(cell => cell.includes(':'))
+        .map(cell => cell.split(':\t').map(part => part.trim()))
+        .filter(row => row[1].length > 1)
+        .reduce((acc, row) => {
+          acc[row[0]] = row[1]
+          return acc
+        }, {}))
+      await nextResource(page)
+    }
+  console.log(resources.map(cell => ({
+      id: cell.ID,
+      title: cell.Title,
+      author: cell.Author,
+      type: cell.GMD,
+      isbn: cell.ISBN,
+      notes: cell.Notes,
+    })))
+
+  } finally {
+    await page.getByRole('button', {name: 'Logout'}).click()
+  }
+})
